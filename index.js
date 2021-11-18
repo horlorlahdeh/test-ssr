8a0983333fb91fad1618b4ed135669d1064b8956
horlorlahdeh <horlorlahdeh@gmail.com>
2021-11-18 12:57:31 +0100

    port

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const path = require('path')
 
 const app = express();
 
+const port = process.env.PORT || 4000
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', async () => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-app.listen(4000, () => {
-    console.log('App listening on port 3000')
+app.listen(port, () => {
+    console.log('App listening on port ' + port)
 })
