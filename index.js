be370cceec47f6eecf42f753a27a65ac67b003b8
horlorlahdeh <horlorlahdeh@gmail.com>
2021-11-18 23:58:14 +0100

    routing

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const port = process.env.PORT || 4000
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', async () => {
+app.get('/', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+app.get('*', async (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
